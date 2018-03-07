28078016c07190898625e5d29aa2ce9a3bfcfa50
dds1999 <29868036+dds1999@users.noreply.github.com>
2018-03-06 23:10:32 -0500

    add json and model classes

---
+++ app/src/main/java/com/example/xu/group59/models/HomelessPerson.java
@@ -0,0 +1,12 @@
+package com.example.xu.group59.models;
+
+/**
+ * Created by Destini on 3/6/2018.
+ */
+
+public class HomelessPerson extends User {
+    public HomelessPerson(String email, String password, String name) {
+        super(email, password, name);
+    }
+
+}
---
+++ app/src/main/java/com/example/xu/group59/models/ShelterEmployee.java
@@ -0,0 +1,11 @@
+package com.example.xu.group59.models;
+
+/**
+ * Created by Destini on 3/6/2018.
+ */
+
+public class ShelterEmployee extends User {
+    public ShelterEmployee(String email, String password, String name) {
+        super(email, password, name);
+    }
+}
